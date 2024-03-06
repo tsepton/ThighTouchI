287b5a7697673c0f04f97ed1f9f71ee1be431333
TheoLelce <theo.leclercq@student.unamur.be>
2024-03-06 14:49:29 +0100

    correction

--- unity/Assets/App/ShowTouchPointsOnCanvas.cs
+++ unity/Assets/App/ShowTouchPointsOnCanvas.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.WSA;
 
 public class ShowTouchPointsOnCanvas : MonoBehaviour {
 
@@ -7,14 +8,17 @@ public class ShowTouchPointsOnCanvas : MonoBehaviour {
     
     private GameObject[] _touchPoints;
     private RectTransform[] _touchPointsRect;
+
+    
     
     private void OnEnable() {
         _touchPoints = new GameObject[10]; // Assuming maximum of 10 touch points
         _touchPointsRect = new RectTransform[10];
-        // Touchpad.OnTouch += DrawTouchPoint ;
+        Touchpad.OnTouch += DrawTouchPoint ;
     }
 
     public void DrawTouchPoint(TouchPoint touchPoint) {
+        Debug.Log("test");
         Vector2 canvasSize = canvas.sizeDelta; // canvasRect.sizeDelta
         Transform parentTransform = canvas.transform; // Parent the cubes to the canvas
 
@@ -22,6 +26,7 @@ public class ShowTouchPointsOnCanvas : MonoBehaviour {
         float touchY = (-0.5f + touchPoint.coordinates.y) * canvasSize.y / 5f;
 
         if (!touchPoint.isLast) {
+            Debug.Log("test2");
             if (_touchPoints[touchPoint.id] == null) {
                 var newPointer = Instantiate(pointer, parentTransform);
                 _touchPoints[touchPoint.id] = newPointer;
