5622a1c48ea07ac92065d0882731210567db468b
Thibaut Septon <tsepton@gmail.com>
2024-04-11 14:55:35 +0200

    Improve UI

--- touch-input/debug-app/src/routes/+page.svelte
+++ touch-input/debug-app/src/routes/+page.svelte
@@ -2,7 +2,9 @@
 	import Main from '$lib/components/Main.svelte';
 </script>
 
-<Main></Main>
+<div id="background">
+	<Main></Main>
+</div>
 
 <style>
 	:global(body) {
@@ -10,16 +12,59 @@
 		margin: 0;
 		padding: 0;
 		height: 100vh;
-		background-image: linear-gradient(to right bottom, #fdfffc, #fbfef9, #f9fdf6, #f6fcf4, #f4fbf1);
 	}
+
+	#background {
+		background: linear-gradient(120deg, #D3D4D9, #FFF9FB, #424C55);
+		background-size: 350% 350%;
+
+		-webkit-animation: AnimationName 50s ease infinite;
+		-moz-animation: AnimationName 50s ease infinite;
+		animation: AnimationName 50s ease infinite;
+	}
+
+	@-webkit-keyframes AnimationName {
+		0% {
+			background-position: 0% 30%;
+		}
+		50% {
+			background-position: 100% 71%;
+		}
+		100% {
+			background-position: 0% 30%;
+		}
+	}
+	@-moz-keyframes AnimationName {
+		0% {
+			background-position: 0% 30%;
+		}
+		50% {
+			background-position: 100% 71%;
+		}
+		100% {
+			background-position: 0% 30%;
+		}
+	}
+	@keyframes AnimationName {
+		0% {
+			background-position: 0% 30%;
+		}
+		50% {
+			background-position: 100% 71%;
+		}
+		100% {
+			background-position: 0% 30%;
+		}
+	}
+
 	:global(.card) {
-		background: rgba(255, 255, 255, 0.2);
+		background: rgba(255, 255, 255, 0.5);
 		border-radius: 16px;
 		box-shadow: 0 4px 30px rgba(0, 0, 0, 0.1);
 		backdrop-filter: blur(5px);
 		-webkit-backdrop-filter: blur(5px);
 		border: 1px solid rgba(255, 255, 255, 0.3);
 		padding-top: 2rem;
-        padding-bottom: 2rem;
+		padding-bottom: 2rem;
 	}
 </style>
