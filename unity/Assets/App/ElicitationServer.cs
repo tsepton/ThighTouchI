84cdef521d8a76adac37545f52e147e771963bd0
Thibaut Septon <tsepton@gmail.com>
2024-06-13 16:34:36 +0200

    Create Elicitation Scene

---
+++ unity/Assets/App/ElicitationServer.cs
@@ -0,0 +1,136 @@
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+using System.Threading;
+using UnityEngine;
+using System.IO;
+using System.Linq;
+
+public class ElicitationServer : MonoBehaviour {
+  private HttpListener listener;
+  private Thread listenerThread;
+  private bool isRunning;
+
+  private List<GameObject> elicitationSample = new();
+  private int currentElicitationIndex = -1;
+
+  private readonly Queue<Action> executionQueue = new();
+
+  void Start() {
+    StartServer();
+    InitElicitation();
+  }
+
+  void OnDestroy() {
+    StopServer();
+  }
+
+  private void Update() {
+    lock (executionQueue) {
+      while (executionQueue.Count > 0) {
+        executionQueue.Dequeue().Invoke();
+      }
+    }
+  }
+
+  public void Enqueue(Action action) {
+    lock (executionQueue) {
+      executionQueue.Enqueue(action);
+    }
+  }
+
+
+  private void InitElicitation() {
+    foreach (Transform child in transform) {
+      elicitationSample.Add(child.gameObject);
+    }
+
+    System.Random rng = new System.Random();
+    elicitationSample = elicitationSample.OrderBy(_ => rng.Next()).ToList();
+  }
+
+  private void ActivateElicitation(int index) {
+    elicitationSample.ForEach(x => x.SetActive(false));
+    if (index < elicitationSample.Count && index > -1)
+      elicitationSample[index].SetActive(true);
+  }
+
+  private void StartServer() {
+    listener = new HttpListener();
+    listener.Prefixes.Add("http://*:8080/");
+    listenerThread = new Thread(HandleRequests);
+    listenerThread.Start();
+    isRunning = true;
+    Debug.Log("Open your browser and load http://localhost:8080/ to change the elicitation.");
+  }
+
+  private void StopServer() {
+    isRunning = false;
+    if (listener != null) {
+      listener.Close();
+      listener = null;
+    }
+
+    if (listenerThread != null) {
+      listenerThread.Abort();
+      listenerThread = null;
+    }
+
+    Debug.Log("Server stopped.");
+  }
+
+  private void HandleRequests() {
+    listener.Start();
+    while (isRunning) {
+      HttpListenerContext context = listener.GetContext();
+      HttpListenerRequest request = context.Request;
+
+      if (request.HttpMethod == "GET")
+        HandleGetRequest(context);
+      else if (request.HttpMethod == "POST")
+        HandlePostRequest(context);
+    }
+
+    listener.Stop();
+  }
+
+  private void HandleGetRequest(HttpListenerContext context) {
+    string filePath = Path.Combine(Application.dataPath, "App/index.html");
+    string responseString = File.Exists(filePath) ? File.ReadAllText(filePath) : "<html><body>Error</body></html>";
+    byte[] buffer = Encoding.UTF8.GetBytes(responseString);
+
+    context.Response.ContentType = "text/html";
+    context.Response.ContentLength64 = buffer.Length;
+    context.Response.OutputStream.Write(buffer, 0, buffer.Length);
+
+    context.Response.OutputStream.Close();
+  }
+
+  private void HandlePostRequest(HttpListenerContext context) {
+    string url = context.Request.Url.AbsolutePath;
+    string response;
+
+    if (url == "/previous/") {
+      currentElicitationIndex = currentElicitationIndex > 0 ? currentElicitationIndex - 1 : 0;
+      Enqueue(() => ActivateElicitation(currentElicitationIndex));
+      response = $"Current: {currentElicitationIndex} out of {elicitationSample.Count}";
+    }
+    else if (url == "/next/") {
+      currentElicitationIndex = currentElicitationIndex < elicitationSample.Count
+        ? currentElicitationIndex + 1
+        : elicitationSample.Count;
+      Enqueue(() => ActivateElicitation(currentElicitationIndex));
+      response = $"Current: {currentElicitationIndex} out of {elicitationSample.Count}";
+    }
+    else response = "Unknown route";
+
+    Debug.Log(response);
+
+    byte[] buffer = Encoding.UTF8.GetBytes($"<html><body>{response}</body></html>");
+
+    context.Response.ContentLength64 = buffer.Length;
+    context.Response.OutputStream.Write(buffer, 0, buffer.Length);
+    context.Response.OutputStream.Close();
+  }
+}
